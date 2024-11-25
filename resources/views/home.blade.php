b2aeec475e09a3e3acb97fcbe0e31e736810a29e
Mirko <mirko.donnarumma@outlook.it>
2024-11-25 23:28:41 +0100

    ultimi aggiornamenti alla pagina home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mirko Donnarumma | Home</title>
+    <title>mirko.dev</title>
     <meta name="description" content="Il portfolio di Mirko Donnarumma, un software developer.">
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
